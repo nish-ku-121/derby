940f82e5b76059eeb8c04802e4bf448453c9f47f
nkumar15-brown-university <43328693+nkumar15-brown-university@users.noreply.github.com>
2020-04-25 20:57:01 -0400

    [#5] Added tests for second price auctions

--- derby/tests/test_auctions.py
+++ derby/tests/test_auctions.py
@@ -4,7 +4,7 @@ from derby.core.auctions import KthPriceAuction
 
 
 
-class TestAuctions(unittest.TestCase):
+class TestFirstPriceAuction(unittest.TestCase):
 
 	def setUp(self):
 		self.auction_items = [
@@ -12,9 +12,7 @@ class TestAuctions(unittest.TestCase):
 						AuctionItem(name="horse2", item_type={"white", "horse"}, owner=None),
 						AuctionItem(name="horse3", item_type={"horse", "white"}, owner=None)
 		]
-		self.first_price_auction = KthPriceAuction(1)
-		self.second_price_auction = KthPriceAuction(2)
-		
+		self.first_price_auction = KthPriceAuction(1)		
 
 	def test_first_price_1(self):
 		auction_items = self.auction_items
@@ -115,6 +113,88 @@ class TestAuctions(unittest.TestCase):
 		self.assertEqual(unallocated, { auction_items[0] : 0.0,
 										auction_items[2] : 0.0 
 		 								})
+
+
+class TestSecondPriceAuction(unittest.TestCase):
+
+	def setUp(self):
+		self.auction_items = [
+						AuctionItem(name="horse1", item_type={"black", "horse"}, owner=None),
+						AuctionItem(name="horse2", item_type={"white", "horse"}, owner=None),
+						AuctionItem(name="horse3", item_type={"horse", "white"}, owner=None)
+		]
+		self.auction = KthPriceAuction(2)
+
+	def test_1(self):
+		auction_items = self.auction_items
+		bids = [
+				Bid("bidder1", auction_items[0], bid_per_item=1.0, total_limit=1.0),
+				Bid("bidder2", auction_items[0], bid_per_item=2.0, total_limit=2.0)
+		]
+
+		results = self.auction.run_auction(bids, auction_items)
+		bid_1_result = results.get_result(bids[0])
+		bid_2_result = results.get_result(bids[1])
+		unallocated = results.get_unallocated()
+		
+		self.assertEqual(bid_1_result, { })
+		self.assertEqual(bid_2_result, { auction_items[0] : 1.0 })
+		self.assertEqual(unallocated, { auction_items[1] : 0.0, 
+									    auction_items[2] : 0.0 })
+
+	def test_2(self):
+		auction_items = self.auction_items
+		bids = [
+				Bid("bidder1", auction_items[0], bid_per_item=1.0, total_limit=1.0),
+				Bid("bidder2", auction_items[1], bid_per_item=2.0, total_limit=2.0)
+		]
+
+		results = self.auction.run_auction(bids, auction_items)
+		bid_1_result = results.get_result(bids[0])
+		bid_2_result = results.get_result(bids[1])
+		unallocated = results.get_unallocated()
+		
+		self.assertEqual(bid_1_result, { auction_items[0] : 0.0 })
+		self.assertEqual(bid_2_result, { auction_items[1] : 0.0 })
+		self.assertEqual(unallocated, { auction_items[2] : 0.0 })
+
+	def test_3(self):
+		auction_items = self.auction_items
+		bids = [
+				Bid("bidder1", auction_items[0], bid_per_item=1.0, total_limit=1.0),
+				Bid("bidder1", auction_items[0], bid_per_item=2.0, total_limit=2.0)
+		]
+
+		results = self.auction.run_auction(bids, auction_items)
+		bid_1_result = results.get_result(bids[0])
+		bid_2_result = results.get_result(bids[1])
+		unallocated = results.get_unallocated()
+		
+		self.assertEqual(bid_1_result, { })
+		self.assertEqual(bid_2_result, { auction_items[0] : 0.0 })
+		self.assertEqual(unallocated, { auction_items[1] : 0.0, auction_items[2] : 0.0 })
+
+	def test_4(self):
+		auction_items = self.auction_items
+		item_to_bids_mapping_func = self.auction.items_to_bids_by_item_type
+		temp_item = AuctionItem(name=None, item_type={"white", "horse"}, owner=None)
+		bids = [
+				Bid("bidder1", temp_item, bid_per_item=1.0, total_limit=1.0),
+				Bid("bidder2", temp_item, bid_per_item=2.0, total_limit=2.0)
+		]
+
+		results = self.auction.run_auction(bids, auction_items, item_to_bids_mapping_func)
+		bid_1_result = results.get_result(bids[0])
+		bid_2_result = results.get_result(bids[1])
+		unallocated = results.get_unallocated()
+		
+		self.assertEqual(bid_1_result, { auction_items[2] : 0.0 })
+		self.assertEqual(bid_2_result, { auction_items[1] : 1.0 })
+		self.assertEqual(unallocated, { auction_items[0] : 0.0 })
+
+		
+
+	
 
 if __name__ == '__main__':
 	unittest.main()
